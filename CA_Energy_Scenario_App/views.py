04fbbf46c2ab379dc108ea763eeb40ab9a299d7e
mikegough <mike.gough@consbio.org>
2016-04-21 15:17:27 -0700

    Code Cleanup (Phase 2b)

--- CA_Energy_Scenario_App/views.py
+++ CA_Energy_Scenario_App/views.py
@@ -1,85 +1,65 @@
 from django.http import HttpResponse
-import urllib
-
 from django.shortcuts import render
 from django.db import connection
-import json
-#The encoder module is used to prevent excessive decimals from being generated when dumping to JSON object
-from json import encoder
-encoder.FLOAT_REPR = lambda o: format(o, '.2f')
-
 from django.views.decorators.gzip import gzip_page
+from django.views.decorators.csrf import csrf_exempt  # Potential security hole.
 
-#Potential security hole.
-from django.views.decorators.csrf import csrf_exempt
+import json
+from json import encoder  # Used to prevent excessive decimals from being generated when dumping to JSON object
+encoder.FLOAT_REPR = lambda o: format(o, '.2f')
 
 @gzip_page
 @csrf_exempt
 def index(request):
-
     if request.method == 'POST':
         WKT = request.POST.get('wktPOST')
-        reporting_units=request.POST.get('reporting_units')
+        reporting_units = request.POST.get('reporting_units')
         solar_slider_value = request.POST.get('solar_slider_value')
+
         ownership_values = request.POST.get('ownership_values')
+        if ownership_values == '':
+            ownership_values='BLM,Military,Native American,Other,Private,State Land,USFS'
+
         distance_to_transmission = request.POST.get('distance_to_transmission')
-        print distance_to_transmission
-        if not distance_to_transmission:
-            distance_to_transmission = ' 999999999999999999999999999999'
-
         distance_to_transmission_units = request.POST.get('distance_to_transmission_units')
-
         if distance_to_transmission_units == 'miles':
-            distance_conversion_factor=1609.344
+            distance_conversion_factor = 1609.344
         else:
-            distance_conversion_factor=1000
+            distance_conversion_factor = 1000
 
-        enable_environment_settings = request.POST.get('enable_environment_settings')
-
-        if enable_environment_settings == '0':
-            ti_slider='99999999'
-            cv_slider='99999999'
-            species_count_slider_value='99999999'
-            chat_slider_value = '-99999999'
-
-        else:
+        enable_environment_settings = request.POST.get('enable_environment_settings') == 'true'
+        if enable_environment_settings:
             ti_slider = request.POST.get('ti_slider')
             cv_slider = request.POST.get('cv_slider')
             species_count_slider_value = request.POST.get('species_count_slider_value')
             chat_slider_value = request.POST.get('chat_slider_value')
 
-        if ownership_values == '':
-            ownership_values='BLM,Military,Native American,Other,Private,State Land,USFS'
-
         exemptions=" and gap_sts NOT IN('1')"
 
-        corridor_avoidance_slider_value = request.POST.get('corridor_avoidance_slider_value')
-        cdfw_slider_value = request.POST.get('cdfw_slider_value')
+        # corridor_avoidance_slider_value = request.POST.get('corridor_avoidance_slider_value')
+        # cdfw_slider_value = request.POST.get('cdfw_slider_value')
 
         min_area = float(request.POST.get('min_area'))
         min_area_units = request.POST.get('min_area_units')
-        #1 square km = 247.105 acres.
 
         if min_area_units == 'meters':
-            area_conversion_factor=1
+            area_conversion_factor = 1
         elif min_area_units == 'acres':
-            area_conversion_factor=4046.86
+            area_conversion_factor = 4046.86
         elif min_area_units == 'hectares':
-            area_conversion_factor=10000
+            area_conversion_factor = 10000
         elif min_area_units == 'square_km':
-            area_conversion_factor=1e+6
+            area_conversion_factor = 1e+6
         elif min_area_units == 'square_mi':
-            area_conversion_factor=2.59e+6
+            area_conversion_factor = 2.59e+6
         elif min_area_units == 'square_feet':
-            area_conversion_factor=0.092903
+            area_conversion_factor = 0.092903
         else:
-            area_conversion_factor=1
+            area_conversion_factor = 1
 
     else:
         WKT=request.GET.get('user_wkt')
         reporting_units=request.GET.get('reporting_units', "onekm")
-
-    ############################################# INPUT PARAMETERS #####################################################
 
     template=request.GET.get('template')
     if not template:
@@ -87,49 +67,31 @@ def index(request):
 
     query_layer="energy_scenario_1km_query_grid"
 
-    #statsFields="intactness"
-    statsFields=None
+    stats_fields=None
 
     if reporting_units == "counties":
-        table="drecp_reporting_units_county_boundaries_no_simplify"
-        featureName="County"
-        featureNamePlural="Counties"
-        categoricalFields="name_pcase"
+        reporting_units_table = "drecp_reporting_units_county_boundaries_no_simplify"
+        name_field = "name_pcase"
 
     if reporting_units == "ecoregion_subareas":
-        table="drecp_reporting_units_ecoregion_subareas_no_simplify"
-        featureName="Ecoregion Subarea"
-        featureNamePlural="Ecoregion Subareas"
-        categoricalFields="sa_name"
+        reporting_units_table = "drecp_reporting_units_ecoregion_subareas_no_simplify"
+        name_field = "sa_name"
 
     if reporting_units == "blm_field_offices":
-        table="drecp_reporting_units_blm_field_offices_no_simplify"
-        featureName="BLM Field Office"
-        featureNamePlural="BLM Field Offices"
-        categoricalFields="fo_name"
+        reporting_units_table = "drecp_reporting_units_blm_field_offices_no_simplify"
+        name_field = "fo_name"
 
     if reporting_units == "deto_recovery_units":
-        table="drecp_reporting_units_deto_recovery_units_no_simplify"
-        featureName="Desert Tortoise Recovery Unit"
-        featureNamePlural="Desert Tortoise Recovery Units"
-        categoricalFields="unit_name"
+        reporting_units_table = "drecp_reporting_units_deto_recovery_units_no_simplify"
+        name_field = "unit_name"
 
     if reporting_units == "huc5_watersheds":
-        table="drecp_reporting_units_huc5_watersheds_1_5_simplify"
-        featureName="HUC5 Watersheds"
-        featureNamePlural="HUC5 Watersheds"
-        categoricalFields="Name"
+        reporting_units_table = "drecp_reporting_units_huc5_watersheds_1_5_simplify"
+        name_field = "Name"
 
     elif reporting_units == "onekm":
-        table="drecp_reporting_units_1km_poly_v2"
-        featureName="1km Reporting Unit"
-        featureNamePlural="1km Reporting Units"
-        categoricalFields="''"
-
-    #areaField="shape_area" #Dataset needs an area field in a PCS (not Lat,Lon).
-    #areaConversionDivisor=1000000 #m2 to km2
-
-    totalArea=1
+        reporting_units_table = "drecp_reporting_units_1km_poly_v2"
+        name_field = "''"
 
     #California
     #initial_lat=37.229722
@@ -140,9 +102,6 @@ def index(request):
     initial_lon=-116.7
 
     zoomLevel = int(request.POST.get('zoomLevel',8))
-
-    #Not used in AWA calculation
-    #PostgresStatsToRetrieve=['avg']
 
     cursor = connection.cursor()
 
@@ -166,40 +125,40 @@ def index(request):
         initialize=0
 
         ################################### BUILD SELECT LIST (FIELDS & TABLES) ########################################
-        selectList="SELECT "
+        select_list="SELECT "
 
-        if statsFields:
+        if stats_fields:
 
-            for field in statsFields.split(','):
-                    selectList+= "sum(" + field + " * shape_area)/sum(shape_area)" + " as " + field + "_" + "avg" + ","
+            for field in stats_fields.split(','):
+                    select_list+= "sum(" + field + " * shape_area)/sum(shape_area)" + " as " + field + "_" + "avg" + ","
 
-        #Aggregates. Count, Unique CSV from categorical fields, Outline of selected features.
-        selectList+="count(*) as count, "
+        # Aggregates: Count, Unique CSV from name field, Outline of selected features.
+        select_list+="count(*) as count, "
 
-        selectList+="string_agg(" + categoricalFields + ", ',') as categorical_values, "
+        select_list+="string_agg(" + name_field + ", ',') as features_names, "
         #Sum of the area of selected features for area weighted average. Maybe report later.
-        #selectList+="sum(shape_area) as sum_area, "
+        #select_list+="sum(shape_area) as sum_area, "
 
-        selectList+="ST_AsText(ST_SnapToGrid(ST_Force_2D(ST_Union(geom)), .0001)) as outline_of_selected_features"
+        select_list+="ST_AsText(ST_SnapToGrid(ST_Force_2D(ST_Union(geom)), .0001)) as outline_of_selected_features"
 
-        tableList=" FROM " + table
+        table_list=" FROM " + reporting_units_table
 
-        selectFieldsFromTable = selectList + tableList
+        select_fields_from_table = select_list + table_list
 
         ############################ "WHERE" (ADD ASPATIAL SEARCH CONDITIONS) ##########################################
         if WKT=='aspatial':
 
-            queryField=request.GET.get('queryField')
+            query_field=request.GET.get('queryField')
             operator=request.GET.get('operator').strip()
-            stringOrValue=request.GET.get('stringOrValue').lower().strip()
+            string_or_value=request.GET.get('stringOrValue').lower().strip()
 
             if operator == "LIKE":
-                selectStatement=selectFieldsFromTable + " where LOWER(" + queryField + ") " +  operator  + "%s"
+                select_statement=select_fields_from_table + " where LOWER(" + query_field + ") " +  operator  + "%s"
             elif operator == ">" or operator == "<":
-                selectStatement=selectFieldsFromTable + " where " + queryField + operator + stringOrValue
+                select_statement=select_fields_from_table + " where " + query_field + operator + string_or_value
             else:
-                selectStatement=selectFieldsFromTable + " where LOWER(" + queryField + ") " + operator + " '" + \
-                                stringOrValue + "'"
+                select_statement=select_fields_from_table + " where LOWER(" + query_field + ") " + operator + " '" + \
+                                string_or_value + "'"
 
         ########################## or "WHERE" (ADD SPATIAL SEARCH CONDITIONS) ##########################################
         else:
@@ -208,28 +167,28 @@ def index(request):
             WKT="SRID=4326;"+WKT
 
             operator=None
-            selectStatement=selectFieldsFromTable + " where ST_Intersects('"+ WKT + "', " + table + ".geom)"
+            select_statement=select_fields_from_table + " where ST_Intersects('"+ WKT + "', " + reporting_units_table + ".geom)"
 
         ######################################## EXECUTE DATABASE QUERY ################################################
 
         if operator == "LIKE":
             try:
-                cursor.execute(selectStatement,['%' + stringOrValue + '%'] )
+                cursor.execute(select_statement,['%' + string_or_value + '%'] )
             except:
                 return render(request, template+'.html', errorHandler(reporting_units, template, initial_lat, initial_lon, 1,0))
-                #cursor.execute(selectStatement,['%' + stringOrValue + '%'] )
+                #cursor.execute(select_statement,['%' + string_or_value + '%'] )
         else:
-            cursor.execute(selectStatement)
+            cursor.execute(select_statement)
             try:
-               cursor.execute(selectStatement)
+               cursor.execute(select_statement)
             except:
                return render(request, template+'.html', errorHandler(reporting_units, template, initial_lat, initial_lon, 1,0))
-               #cursor.execute(selectStatement)
+               #cursor.execute(select_statement)
 
-        print selectStatement
+        print select_statement
         ################################# STORE COLUMN, VALUE PAIRS IN A DICT ##########################################
 
-        resultsDict={}
+        results_dict={}
 
         #Get field names
         columns = [colName[0] for colName in cursor.description]
@@ -238,46 +197,46 @@ def index(request):
             for row in cursor:
                 for i in range(len(row)):
                     if isinstance(row[i], basestring):
-                        resultsDict[columns[i]] = row[i].strip()
+                        results_dict[columns[i]] = row[i].strip()
                     else:
-                        resultsDict[columns[i]] =(float(round(row[i],2)))
+                        results_dict[columns[i]] =(float(round(row[i],2)))
         except:
             return render(request, template+'.html', errorHandler(reporting_units, template, initial_lat, initial_lon, 0,1))
 
-        #return HttpResponse(resultsDict['sum_area'])
+        #return HttpResponse(results_dict['sum_area'])
 
-        WKT_SearchArea=resultsDict['outline_of_selected_features']
-        WKT_SearchArea=WKT_SearchArea.replace('%', ' ')
-        WKT_SearchArea="SRID=4326;"+WKT_SearchArea
+        WKT_search_area=results_dict['outline_of_selected_features']
+        WKT_search_area=WKT_search_area.replace('%', ' ')
+        WKT_search_area="SRID=4326;"+WKT_search_area
 
-        categoricalValues=[]
-        categoricalValues.extend(resultsDict['categorical_values'].split(','))
-        categoricalValues=list(set(categoricalValues))
-        categoricalValues.sort()
+        features_names=[]
+        features_names.extend(results_dict['features_names'].split(','))
+        features_names=list(set(features_names))
+        features_names.sort()
 
         #Remove these from the Dictionary before dumping to a JSON object (causing error & no need to send twice).
-        resultsDict.pop('outline_of_selected_features',0)
-        resultsDict.pop('categorical_values',0)
+        results_dict.pop('outline_of_selected_features',0)
+        results_dict.pop('features_names',0)
 
-        count=int(resultsDict["count"])
+        count=int(results_dict["count"])
 
         #Take fieldname,value pairs from the dict and dump to a JSON string.
-        resultsJSON=json.dumps(resultsDict)
-
-        ##################################### SET ADDITIONAL VARIABLES #################################################
-
-        if count > 1 :
-            featureName=featureNamePlural
-
-        if not featureName:
-            featureName="Counties"
+        results_json=json.dumps(results_dict)
 
         ################################## GRID CELL Query (Find suitable polygons) ####################################
 
         cursor = connection.cursor()
 
         #initial query. Find suitable polygons
-        query1="create temp table temp1 as SELECT ST_Union(geom) as the_geom from " +  query_layer + " where intactness <=" + ti_slider + "and hi_linkage <=" + cv_slider + " and speciescou <=" + species_count_slider_value + " and ch_rank >=" + chat_slider_value + " and dniann >=" + solar_slider_value + " and dist_trans <= " + str(float(distance_to_transmission) * distance_conversion_factor) + " and ownership = ANY('" + "{" + ownership_values + "}" + "'::text[]) " + "and ST_Intersects('"+ WKT_SearchArea + "', " + query_layer + ".geom)" + exemptions
+        query1="create temp table temp1 as SELECT ST_Union(geom) as the_geom from " +  query_layer + " where dniann >=" + solar_slider_value + " and ownership = ANY('" + "{" + ownership_values + "}" + "'::text[]) "
+
+        if enable_environment_settings:
+            query1+=" and intactness <=" + ti_slider + "and hi_linkage <=" + cv_slider + " and speciescou <=" + species_count_slider_value + " and ch_rank >=" + chat_slider_value
+        if distance_to_transmission:
+            query1+=" and dist_trans <= " + str(float(distance_to_transmission) * distance_conversion_factor)
+        if WKT:
+            query1+="and ST_Intersects('"+ WKT_search_area + "', " + query_layer + ".geom)" + exemptions
+
         cursor.execute(query1)
 
         #explode to isolate smaller shapes
@@ -314,39 +273,33 @@ def index(request):
             return render(request, template+'.html', errorHandler(reporting_units, template, initial_lat, initial_lon, 0,1))
 
 
-        WKT_SelectedPolys=onekmDict['outline_of_selected_features']
-        #The version of PostGIS on Webfaction was returning SRID=4326 in the multipolygon (check the console for last_poly). It was causing Leaflet to Break. t is null. This was the solution.
-        WKT_SelectedPolys=WKT_SelectedPolys.replace('SRID=4326;','')
+        WKT_selected_polys=onekmDict['outline_of_selected_features']
+        # The version of PostGIS on Webfaction was returning SRID=4326 in the multipolygon (check the console for last_poly). It was causing Leaflet to Break. t is null. This was the solution.
+        WKT_selected_polys=WKT_selected_polys.replace('SRID=4326;','')
 
-
-        area_query="SELECT ST_Area(ST_GeogFromText("+"'"+WKT_SelectedPolys+"'"+")) as total_area"
+        area_query="SELECT ST_Area(ST_GeogFromText("+"'"+WKT_selected_polys+"'"+")) as total_area"
         cursor.execute(area_query)
         area_meters=cursor.fetchone()[0]
-        area_km=area_meters*1000000.0
         area_acres=area_meters*0.0002471044
-
+        # area_km=area_meters*1000000.0
 
         if reporting_units=='onekm':
-            WKT_SearchArea=WKT
-
-
+            WKT_search_area=WKT
 
         context={'template': template,
                  'initialize': initialize,
-                 'WKT_SelectedPolys': WKT_SelectedPolys,
-                 'WKT_SearchArea': WKT_SearchArea,
+                 'WKT_SelectedPolys': WKT_selected_polys,
+                 'WKT_SearchArea': WKT_search_area,
                  'reporting_units': reporting_units,
-                 'featureName': featureName,
-                 'featureNamePlural': featureNamePlural,
                  'totalArea': area_acres,
                  'zoomLevel': zoomLevel, 'count': count,
                  'initial_lat':initial_lat,
                  'initial_lon': initial_lon,
-                 'resultsJSON': resultsJSON,
-                 'categoricalValues': categoricalValues,
+                 'resultsJSON': results_json,
+                 'categoricalValues': features_names,
                  'error': 0,
-                 'ti_slider':ti_slider,
                  }
+
     if request.method == 'POST':
         return HttpResponse(json.dumps(context))
     else:
@@ -362,7 +315,7 @@ def errorHandler(reporting_units, template, initial_lat, initial_lon, error, sel
              'initial_lat': initial_lat,
              'initial_lon': initial_lon,
              #Need this Below
-             'resultsJSON': resultsJSON,
+             'resultsJSON': results_json,
              'count': 0,
              'error': error,
              'selectionWarning':selectionWarning,
